fa9c3551417e5b519e57f6f7689f78add13b0a46
VahAvetisyan <vah.avetisyan@bk.ru>
2023-08-18 02:35:42 +0400

    responsive

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,11 @@ export default function About() {
   return (
     <div id='about-component'>
       <div id='about'>
-        <h2>Hello, I'm Vahan Avetisyan, a 24-year-old web developer from Armenia. I studied development at Armenian Code Academy and have a strong foundation in HTML, CSS, and JavaScript. I specialize in React JS and have experience with Redux, Material UI, Firebase, Trello, Jira, React Router Dom, Git, and GitHub.
+        <p>Hello, I'm Vahan Avetisyan, a 24-year-old web developer from Armenia. I studied development at Armenian Code Academy and have a strong foundation in HTML, CSS, and JavaScript. I specialize in React JS and have experience with Redux, Material UI, Firebase, Trello, Jira, React Router Dom, Git, and GitHub.
 
 Throughout my journey, I've worked on various projects, crafting responsive and visually appealing user interfaces. I enjoy solving complex problems and creating seamless user experiences. My passion for continuous learning drives me to stay updated with the latest trends in web development.
 
-Feel free to explore my portfolio to see examples of my work. If you have any exciting opportunities or projects you'd like to collaborate on, I'd be thrilled to hear from you. Let's connect and build something great together!</h2>
+Feel free to explore my portfolio to see examples of my work. If you have any exciting opportunities or projects you'd like to collaborate on, I'd be thrilled to hear from you. Let's connect and build something great together!</p>
 
       </div>
     </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,18 +3,22 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 
 export default function Contact() {
 
 
   return (
-    <div className="container">
-      {/* <div><a href='tel:+37455990103'>(+374) 55 99 01 03</a></div>
-      <div><a href='https://mail.google.com/mail/u/0/#inbox?compose=CllgCJNsvTrfGGjtGSvsJgNdcxtwcBQvxHdSRwQrxZdLBMPnvkBmCVkblwFTsCpsftrZkTkPXlq'>vahan.avetisyan.ait@gmail.com</a></div> */}
-      <div id="instagram"><a href="https://www.instagram.com/vah_avetisian/" target="_blank"><InstagramIcon /></a></div>
-      <div id="facebook"><a href="https://www.facebook.com/vah.avetisian" target="_blank"><FacebookIcon /></a></div>
-      <div id="linkedin"><a href="https://www.linkedin.com/in/vah-avetisyan-3366a7262/" target="_blank"><LinkedInIcon /></a></div>
-      <div id="github"><a href="https://github.com/VahAvetisyan" target="_blank"><GitHubIcon /></a></div>
+    <div id="contacts">
+    <div className="socials">
+      <a href="https://www.instagram.com/vah_avetisian/" target="_blank"><button id="instagram"><InstagramIcon /></button></a>
+      <a href="https://www.facebook.com/vah.avetisian" target="_blank"><button id="facebook"><FacebookIcon /></button></a>
+      <a href="https://www.linkedin.com/in/vah-avetisyan-3366a7262/" target="_blank"><button id="linkedin"><LinkedInIcon /></button></a>
+      <a href="https://github.com/VahAvetisyan" target="_blank"><button id="github"><GitHubIcon /></button></a>
+    </div>
+    <p><LocalPhoneIcon /> +374 55 99 01 03</p>
+    <p><AlternateEmailIcon /> vahan.avetisyan.ait@gmail.com</p>
     </div>
   );
 }
